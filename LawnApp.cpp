0c25ebb24c59d4cd983fbb9798f0b7756c23de79
BULLETBOT <csgodenisz@gmail.com>
2024-06-26 15:56:05 +0200

    Update to v1.4.6.1

--- LawnApp.cpp
+++ LawnApp.cpp
@@ -129,7 +129,7 @@ LawnApp::LawnApp()
 	mDebugKeysEnabled = false;
 	isFastMode = false;
 	mProdName = "PlantsVsZombies";
-	mVersion = "v1.4.6";
+	mVersion = "v1.4.6.1";
 	mReconVersion = "PvZ: QoTL " + mVersion;
 	std::string aTitleName = "Plants vs. Zombies: QoTL";
 	aTitleName += " " + mVersion;
