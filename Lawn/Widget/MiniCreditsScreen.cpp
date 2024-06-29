e70e607ad54184ce24a9681fa3a4771263fa64c9
BULLETBOT <csgodenisz@gmail.com>
2024-06-29 02:54:36 +0200

    Fix MiniCredits Screen

--- Lawn/Widget/MiniCreditsScreen.cpp
+++ Lawn/Widget/MiniCreditsScreen.cpp
@@ -102,6 +102,8 @@ void MiniCreditsScreen::ButtonPress(int theId)
 
 void MiniCreditsScreen::Update()
 {
+	Widget::Update();
+	MarkDirty();
 	mBackButton->Update();
 	if (mApp->HasFinishedAdventure())
 		mMusicButton->Update();
