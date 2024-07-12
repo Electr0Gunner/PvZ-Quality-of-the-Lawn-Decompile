36c6594e4ae082a9e0cdf40d1368e9cbe452eb63
BULLETBOT <csgodenisz@gmail.com>
2024-07-12 18:49:54 +0200

    Fixed Backup and Disco Zombies

--- Lawn/Zombie.cpp
+++ Lawn/Zombie.cpp
@@ -3313,9 +3313,16 @@ void Zombie::DropHead(unsigned int theDamageFlags)
     }
     if (mZombieType == ZombieType::ZOMBIE_DANCER)
     {
+        ReanimShowPrefix("Zombie_disco_glasses", RENDER_GROUP_HIDDEN);
+        ReanimShowPrefix("Zombie_disco_chops", RENDER_GROUP_HIDDEN);
         aRenderOrder = mRenderOrder - 1;
     }
-    if (mZombieType == ZombieType::ZOMBIE_NEWSPAPER)
+    else if (mZombieType == ZombieType::ZOMBIE_BACKUP_DANCER)
+    {
+        ReanimShowPrefix("Zombie_backup_stash", RENDER_GROUP_HIDDEN);
+        ReanimShowPrefix("Zombie_disco_chops", RENDER_GROUP_HIDDEN);
+    }
+    else if (mZombieType == ZombieType::ZOMBIE_NEWSPAPER)
     {
         aEffect = ParticleEffect::PARTICLE_ZOMBIE_NEWSPAPER_HEAD;
     }
@@ -3350,7 +3357,6 @@ void Zombie::DropHead(unsigned int theDamageFlags)
         }
         else if (mZombieType == ZombieType::ZOMBIE_BACKUP_DANCER)
         {
-            ReanimShowPrefix("anim_earing", RENDER_GROUP_HIDDEN);
             aParticle->OverrideImage(nullptr, IMAGE_ZOMBIEBACKUPDANCERHEAD);
         }
         else if (mZombieType == ZombieType::ZOMBIE_BOBSLED)
@@ -3465,6 +3471,12 @@ void Zombie::SetupReanimForLostArm(unsigned int theDamageFlags)
     case ZombieType::ZOMBIE_POLEVAULTER:
         ReanimShowTrack("Zombie_polevaulter_outerarm_lower", RENDER_GROUP_HIDDEN);
         ReanimShowTrack("Zombie_outerarm_hand", RENDER_GROUP_HIDDEN);
+        break;
+    case ZombieType::ZOMBIE_DANCER:
+    case ZombieType::ZOMBIE_BACKUP_DANCER:
+        ReanimShowTrack("Zombie_disco_outerarm_lower", RENDER_GROUP_HIDDEN);
+        ReanimShowTrack("Zombie_disco_outerhand", RENDER_GROUP_HIDDEN);
+        ReanimShowTrack("Zombie_disco_outerhand_point", RENDER_GROUP_HIDDEN);
         break;
     default:
         ReanimShowPrefix("Zombie_outerarm_lower", RENDER_GROUP_HIDDEN);
